0a5a00938bc7d992e0604f62ad9d9a8cdef402bd
KauanHK <kauankaestner06@gmail.com>
2025-02-12 21:59:43 -0300

    Adicionado manager

---
+++ manager/__init__.py
@@ -0,0 +1,13 @@
+from .game import Game, QuitPygame, SwitchInterface
+from .interface import Interface
+from .event import Event, Events
+
+
+__all__ = [
+    "Event",
+    "Events",
+    "Game",
+    "Interface",
+    "QuitPygame",
+    "SwitchInterface"
+]
