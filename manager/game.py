e4e420e697deb87d7c5c669f9d15fb3cf32fb874
KauanHK <kauankaestner06@gmail.com>
2025-02-14 09:35:31 -0300

    feat(Game): Adicionado frame global

--- manager/game.py
+++ manager/game.py
@@ -11,9 +11,12 @@ class Game:
         
         self._interfaces: list[Interface] = []
         self._current_interface: Interface | None = None
-        self._current_event_types: tuple[int, ...] = ()
-        self._global_event_types: tuple[int, ...] = ()
+
+        self._event_types: tuple[int, ...] = ()
+
         self._events: dict[int, list[Event]] = {}
+        self._frame = None
+
         if quit:
             self._events[pg.QUIT] = [Event(quit_pygame)]
 
@@ -26,6 +29,10 @@ class Game:
             return f
 
         return decorator
+    
+    def frame(self, func: Callable[[pg.Surface], None]) -> Callable:
+        self._frame = func
+        return func
 
     def register_interface(self, interface: Interface) -> None:
 
@@ -40,35 +47,41 @@ class Game:
 
         pg.init()
         try:
-            self._global_event_types = tuple(self._events.keys())
-            self._current_event_types = self._current_interface.get_event_types()
+            self._event_types = self._get_event_types()
             self._run(screen)
         except QuitPygame: ...
         except KeyboardInterrupt:
             print()
         finally:
             pg.quit()
+
+    def _get_event_types(self) -> tuple[str, ...]:
+        return tuple(set(*self._events.keys(), *self._current_interface.get_event_types()))
     
     def _run(self, screen: pg.Surface) -> None:
 
         clock = pg.time.Clock()
         while True:
+            
             try:
-                self._frame(screen, clock)
+                self._run_events()
+                self._run_frame(screen)
             except SwitchInterface as e:
                 self._current_interface = get_interface(e.name)
-                self._current_event_types = self._current_interface.get_event_types()
+                self._current_event_types = self._get_event_types()
 
-    def _frame(self, screen: pg.Surface, clock: pg.time.Clock) -> None:
-        
-        for pygame_event in pg.event.get(self._global_event_types):
-            for event in self._events[pygame_event.type]:
+            clock.tick(60)
+            pg.display.flip()
+
+    def _run_events(self) -> None:
+
+        for pygame_event in pg.event.get(self._event_types):
+            for event in self._events.get(pygame_event.type, []):
                 event.run(pygame_event)
+                self._current_interface.run_event(pygame_event)
 
-        for pygame_event in pg.event.get(self._current_event_types):
-            self._current_interface.run_event(pygame_event)
-        
+    def _run_frame(self, screen: pg.Surface) -> None:
+
+        if self._frame is not None:
+            self._frame(screen)
         self._current_interface.run_frame(screen)
-
-        clock.tick(60)
-        pg.display.flip()
