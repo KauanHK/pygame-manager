713e8bc594ba4767f40a9584c182befa79f0ffea
KauanHK <kauankaestner06@gmail.com>
2025-02-14 10:09:08 -0300

    refactor: Criado BaseInterface

--- manager/interface.py
+++ manager/interface.py
@@ -3,24 +3,22 @@ from .event import Events
 from typing import Callable, Self
 
 
-class Interface:
+class BaseInterface:
 
-    _objects: dict[str, Self] = {}
+    def __init__(self) -> None:
 
-    def __init__(self, name: str) -> None:
-
-        self._name = name
         self._events = Events()
         self._frame = None
-        Interface._objects[name] = self
+        self._event_types: set[int] = set()
 
-    def get_name(self) -> str:
-        return self._name
+    def get_event_types(self) -> tuple[int]:
+        return tuple(self._event_types)
 
     def event(self, event_type: int, params: tuple[str, ...] = (), **kwargs) -> Callable[[Callable], Callable]:
 
         def decorator(f: Callable) -> Callable:
             self._events.add(f, event_type, params, **kwargs)
+            self._event_types.add(event_type)
             return f
 
         return decorator
@@ -32,14 +30,25 @@ class Interface:
     def frame(self, frame: Callable) -> None:
         self._frame = frame
 
-    def get_event_types(self) -> tuple[int, ...]:
-        return self._events.get_event_types()
-
     def run_event(self, pygame_event: pg.event.Event) -> None:
         self._events.run(pygame_event)
 
     def run_frame(self, screen: pg.Surface) -> None:
         self._frame(screen)
+        
+
+class Interface(BaseInterface):
+
+    _objects: dict[str, Self] = {}
+
+    def __init__(self, name: str) -> None:
+
+        super().__init__()
+        self._name = name
+        Interface._objects[name] = self
+
+    def get_name(self) -> str:
+        return self._name
 
     def __repr__(self) -> str:
         return f'Interface({self._name})'
