0e789910fb5d711bf927b572096bfbd7d4de109e
KauanHK <kauankaestner06@gmail.com>
2025-02-28 16:49:13 -0300

    Refactor: utils.py contém apenas funções auxiliares

--- pygame_manager/utils.py
+++ pygame_manager/utils.py
@@ -1,25 +1,10 @@
-import pygame as pg
-from typing import Callable, NoReturn, TypeVar, Any
+from .exceptions import QuitPygame, SwitchInterface
+from typing import NoReturn
 
-
-FuncEvent = TypeVar('FuncEvent', bound = Callable[..., Any])
-FuncFrame = TypeVar('FuncFrame', bound = Callable[[pg.Surface], Any])
-EventsClass = TypeVar('EventsClass', bound = type)
-
-class QuitPygame(BaseException): ...
 
 def quit_pygame(*args) -> NoReturn:
     raise QuitPygame(*args)
 
-class SwitchInterface(BaseException):
 
-    def __init__(self, name: str) -> None:
-        self.name = name
-
-
-def switch_interface(name: str) -> Callable[[], NoReturn]:
+def switch_interface(name: str) -> NoReturn:
     raise SwitchInterface(name)
-
-
-class ActivatedInterfaceError(BaseException): ...
-class DeactivatedInterfaceError(BaseException): ...
