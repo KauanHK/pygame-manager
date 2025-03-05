1c9776fb9ed8e59430b9581e9042addf253872d7
KauanHK <kauankaestner06@gmail.com>
2025-03-05 08:37:26 -0300

    feat: Adicionado EventsManager()

--- pygame_manager/event.py
+++ pygame_manager/event.py
@@ -1,4 +1,6 @@
 import pygame as pg
+from pygame_manager.types import EventsClass, FuncEvent
+from functools import wraps
 from typing import Callable, Any
 
 
@@ -102,3 +104,86 @@ class LoadingEvent(BaseEvent):
         for obj in objects:
             events.append(Event(getattr(obj, self.func.__name__), self.params, **self.kwargs))
         return events
+
+
+class EventsManager:
+
+    def __init__(self) -> None:
+
+        super().__init__()
+        self._loading_events: dict[str, list[LoadingEvent]] = {}
+        self._classes: dict[str, type[EventsClass]] = {}
+        self._objects: dict[str, list[object]] = {}
+        self._events: dict[int, list[Event]] = {}
+        self._is_init: bool = False
+
+    def is_init(self) -> bool:
+        return self._is_init
+
+    def init(self) -> None:
+
+        self._events.clear()
+        for cls in map(lambda c: self._classes.get(c), self._loading_events):
+            for le in self._loading_events[cls.__qualname__]:
+                events: list[Event] = le.load(cls, self._objects[cls.__qualname__])
+                self._events.get(le.event_type, []).extend(events)
+
+    def add_event(self, func: FuncEvent, event_type: int, params: tuple[str, ...] = (), **kwargs: Any) -> None:
+
+        le: LoadingEvent = LoadingEvent(func, event_type, params, **kwargs)
+        self._loading_events.get(le.owner_qualname, []).append(le)
+
+    def register_cls(self, cls: type[EventsClass]) -> type[EventsClass]:
+        """Registra uma classe para carregar eventos dela.
+
+        Necessário para registrar eventos como métodos e chamá-los passando
+        a instância como primeiro parâmetro. Decora o seu __init__ para
+        registrar todas as instâncias criadas.
+
+        :param type cls: A classe a ser registrada.
+        :return: A própria classe.
+        """
+
+        self.set_cls(cls)
+
+        original_init = cls.__init__
+
+        @wraps(original_init)
+        def __init__(*args, **kwargs) -> None:
+            original_init(*args, **kwargs)
+            self.add_object(args[0])
+
+        cls.__init__ = __init__
+        return cls
+
+    def set_cls(self, cls: type[EventsClass]) -> None:
+        """Registra uma classe para carregar seus eventos e retorna None.
+
+        A diferença para register_cls() é que o __init__ da classe não é decorado,
+        portanto não garante que os eventos de instâncias dessa classe sejam carregados.
+        Use set_cls para registrar classes com eventos em múltiplas interfaces, ou
+        use o decorador Group.register_cls().
+
+        :param cls: A classe a ser registrada.
+        :return: None.
+        """
+        self._classes[cls.__qualname__] = cls
+        self._objects[cls.__qualname__] = []
+
+    def add_object(self, obj: object) -> None:
+        """Adiciona um objeto a lista de objetos de sua classe.
+
+        É necessário registrar a classe antes de adicionar o
+        objeto, caso contrário, lança um KeyError.
+        Ao registrar um objeto, seus eventos serão executados.
+        Para adicionar todos os objetos automaticamente, use
+        o decorador Interface.register_cls() ou Group.register_cls().
+
+        :param obj: O objeto a ser adicionado.
+        """
+        self._objects[obj.__class__.__qualname__].append(obj)
+
+    def run(self, pygame_event: pg.event.Event) -> None:
+
+        for event in self._events.get(pygame_event.type, []):
+            event.run(pygame_event)
